cba43600ac71d996e8f313aacf63082c145869cb
akilaanjanadissanayaka <akiladissanayaka255@gmail.com>
2023-02-25 00:47:47 +0530

    predict divorce

---
+++ app.py
@@ -0,0 +1,46 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html', prediction_text="Shall we verify?")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    print(int_features)
+    final_features = [np.array(int_features)]
+    print(final_features)
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 3)
+    text=""
+    if output==1:
+        text=" Divorce May be the Future of Your Marriage."
+    else:
+        text=" Happily Ever After May be in the Future of Your Marriage"
+    print(text)
+
+    return render_template('index.html', prediction_text='Based on Your Answers: {}'.format(text))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+import pickle
+warnings.filterwarnings("ignore")
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report, plot_roc_curve, confusion_matrix, ConfusionMatrixDisplay
+
+
+df = pd.read_csv('divorce_data.csv', sep = ';')
+# print(df.head())
+
+x_train, x_test, y_train, y_test = train_test_split(df.drop('Divorce', axis=1),
+                                                   df['Divorce'],
+                                                   test_size = 0.25,
+                                                   random_state=42)
+
+model_lr = LogisticRegression().fit(x_train, y_train)
+y_pred_lr = model_lr.predict(x_test)
+
+print('Accuracy score LR:  {:.4f}' .format(accuracy_score(y_test, y_pred_lr)))
+
+
+
+# predictions = model_lr.predict([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,4,2,0,0,0,0,0,0,0,0,1,4,2,0,4,2,1,1,0,0,0,1,0,0]])
+#
+# print(predictions)
+
+# Saving model to disk
+pickle.dump(model_lr, open('model.pkl','wb'))
+
+# Loading model to compare the results
+model = pickle.load(open('model.pkl','rb'))
+print(model.predict([[2,2,4,1,0,0,0,0,0,0,1,0,1,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,2,1,2,0,1,2,1,3,3,2,1,1,2,3,2,1,3,3,3,2,3,2,1]]))
+
+
